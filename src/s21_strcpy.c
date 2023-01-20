4c3b8275c89fb8dc09ecc8d10985104a991fd957
Arnold Rhyhorn <arnoldrh@at-f4.msk.21-school.ru>
2023-01-20 22:06:52 +0300

    strcpy and strncpy

---
+++ src/s21_strcpy.c
@@ -0,0 +1,13 @@
+#include "s21_string.h"
+
+char *s21_strcpy(char *dest, const char *src) {
+  int save_i = 0;
+  int i = 0;
+  for (i = 0; src[i] != '\0'; i++) {
+    dest[i] = src[i];
+  }
+  if (save_i == 0) save_i = i;
+  if (src[i] == '\0') dest[i] = '\0';
+  printf("%s", dest);
+  return dest;
+}
--- src/s21_string.h
+++ src/s21_string.h
@@ -8,5 +8,7 @@
 char *s21_strerror(int errnum);
 int s21_strncmp(const char *str1, const char *str2, size_t n);
 int s21_strcmp(const char *str1, const char *str2);
+char *s21_strcpy(char *dest, const char *src);
+char *s21_strncpy(char *dest, const char *src, size_t n);
 
 #endif
