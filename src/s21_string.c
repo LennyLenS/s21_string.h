824b2d3a9f900e3d5764ae615fecaf5edc3d534b
Bank Blackberries <bankblac@vo-k3.msk.21-school.ru>
2023-01-23 00:08:12 +0300

    strstr

--- src/s21_string.c
+++ src/s21_string.c
@@ -62,3 +62,20 @@ char *s21_strtok(char *str, const char *delim) {
     str_cash = &str[str_ind];
     return &str[ret_ind];
 }
+
+char *s21_strstr(const char *haystack, const char *needle) {
+    if(*needle == '\0') return (char*)haystack;
+    int i = 0;
+    while(haystack[i] != '\0'){
+        int j = 0;
+        int k = i;
+        while(needle[j] != '\0' || haystack[k] != '\0'){
+            if(needle[j] != haystack[k]) break;
+            k++;
+            j++;
+        }
+        if(needle[j] == '\0') return (char*)haystack + i;
+    i++;
+    }
+    return NULL;
+}
