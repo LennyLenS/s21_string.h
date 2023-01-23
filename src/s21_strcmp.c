b9b235948ce3fa512955d9642b13b510c7caa5ac
Arnold Rhyhorn <arnoldrh@at-f4.msk.21-school.ru>
2023-01-23 18:07:05 +0300

    tests is ready except strerror because need sprintf

--- src/s21_strcmp.c
+++ src/s21_strcmp.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "s21_string.h"
 // Сравнивает строку, на которую указывает str1, со строкой, на которую
 // указывает str2.
@@ -11,6 +13,5 @@ int s21_strcmp(const char *str1, const char *str2) {
     if (*str1 == '\0' && *str2 == '\0') flag = false;
   }
   res = *str1 - *str2;
-  // printf("%d", res);
   return res;
 }
