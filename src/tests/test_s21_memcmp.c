2f6e701e15be21d39f2e67acd9dc8391c8d9f389
urtotsma <adminnn@LAPTOP-7GGNOOMC.localdomain>
2023-02-03 16:26:51 +0300

    fix test memcmp

--- src/tests/test_s21_memcmp.c
+++ src/tests/test_s21_memcmp.c
@@ -6,24 +6,21 @@ START_TEST(s21_memcmp_test) {
   char *test_3 = "ASDF";
   char *test_4 = "true\0fail";
 
-  ck_assert_int_eq(s21_memcmp(test_1, "5", 1), memcmp(test_1, "5", 1));
-  ck_assert_int_eq(s21_memcmp(test_1, "111", 3), memcmp(test_1, "111", 3));
-  ck_assert_int_eq(s21_memcmp(test_1, "123456", 6),
-                   memcmp(test_1, "123456", 6));
-  ck_assert_int_eq(s21_memcmp(test_1, "12345", 5), memcmp(test_1, "12345", 5));
+  ck_assert_int_gt(s21_memcmp(test_1, "5", 1) * memcmp(test_1, "5", 1), 0);
+  ck_assert_int_gt(s21_memcmp(test_1, "111", 3) * memcmp(test_1, "111", 3), 0);
+  ck_assert_int_eq(s21_memcmp(test_1, "123456", 6) * memcmp(test_1, "123456", 6), 0);
+  ck_assert_int_eq(s21_memcmp(test_1, "12345", 5) * memcmp(test_1, "12345", 5), 0);
 
-  ck_assert_int_eq(s21_memcmp(test_2, " ", 1), memcmp(test_2, " ", 1));
-  ck_assert_int_eq(s21_memcmp(test_2, " ", 2), memcmp(test_2, " ", 2));
-  ck_assert_int_eq(s21_memcmp(test_2, "1234", 3), memcmp(test_2, "1234", 3));
+  ck_assert_int_eq(s21_memcmp(test_2, " ", 1) * memcmp(test_2, " ", 1), 0);
+  ck_assert_int_gt(s21_memcmp(test_2, " ", 2) * memcmp(test_2, " ", 2), 0);
+  ck_assert_int_gt(s21_memcmp(test_2, "1234", 3)*memcmp(test_2, "1234", 3), 0);
 
-  ck_assert_int_eq(s21_memcmp(test_3, "ASDF", 4), memcmp(test_3, "ASDF", 4));
-  ck_assert_int_eq(s21_memcmp(test_3, "asdf", 4), memcmp(test_3, "asdf", 4));
-  ck_assert_int_eq(s21_memcmp(test_3, "aaaaaa", 6),
-                   memcmp(test_3, "aaaaaa", 6));
+  ck_assert_int_eq(s21_memcmp(test_3, "ASDF", 4)* memcmp(test_3, "ASDF", 4), 0);
+  ck_assert_int_gt(s21_memcmp(test_3, "asdf", 4) * memcmp(test_3, "asdf", 4), 0);
+  ck_assert_int_gt(s21_memcmp(test_3, "aaaaaa", 6) * memcmp(test_3, "aaaaaa", 6), 0);
 
-  ck_assert_int_eq(s21_memcmp(test_4, "\0", 1), memcmp(test_4, "\0", 1));
-  ck_assert_int_eq(s21_memcmp(test_4, "true\0", 5),
-                   memcmp(test_4, "true\0", 5));
+  ck_assert_int_gt(s21_memcmp(test_4, "\0", 1) * memcmp(test_4, "\0", 1), 0);
+  ck_assert_int_eq(s21_memcmp(test_4, "true\0", 5) * memcmp(test_4, "true\0", 5), 0);
 }
 END_TEST
 
