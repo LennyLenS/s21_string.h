f3ea34b757a18d74159e5f7050dbfd709b7522a6
urtotsma <adminnn@LAPTOP-7GGNOOMC.localdomain>
2023-02-12 16:08:19 +0300

    1

--- src/sub_func.c
+++ src/sub_func.c
@@ -5,62 +5,39 @@ void shift_str(char *str, int size){
 		str[i] = str[i - 1];
 	}
 }
-char *DecInHex(int n){
-	int size = (int)ceil(log2(n + 1) / 4) + 1;
-	char *ans = (char *)malloc(size);
-	int pos = size - 2;
-	while(n > 0){
-		int residue = n % 16;
-		n /= 16;
-		if(residue <= 9){
-			ans[pos--] = residue + '0';
-		}else{
-			ans[pos--] = residue - 10 + 'A';
-		}
-	}
-	ans[size - 1] = '\0';
-	return ans;
-}
 
-char *DecInOct(int n){
-	int size = (int)ceil(log2(n + 1) / 3) + 1;
-	char *ans = (char *)malloc(size);
-	int pos = size - 2;
-	while(n > 0){
-		int residue = n % 8;
-		n /= 8;
-		ans[pos--] = residue + '0';
-	}
-	ans[size - 1] = '\0';
-	return ans;
-}
-
-char *DecInBin(int n){
-	int size = (int)ceil(log2(n + 1)) + 1;
-	char *ans = (char *)malloc(size);
-	int pos = size - 2;
-	while(n > 0){
-		int residue = n % 2;
-		n /= 2;
-		ans[pos--] = residue + '0';
-	}
-	ans[size - 1] = '\0';
-	return ans;
-
-}
-
-void UDecInStr(char *buff, unsigned int n){
+void UDecInNumSys(char *buff, unsigned int n, int mes){
 	int size_ans = 0;
 	while(n != 0){
 		size_ans += 1;
 		shift_str(buff, size_ans);
-		ans[0] = n % 10 + '0';
-		n /= 10;
+		if(n % mes > 9){
+			ans[0] = n % mes - 10 + 'A';
+		}else{
+			ans[0] = n % mes + '0';
+		}
+		n /= mes;
 	}
 	buff[size_ans + 1] = '\0';
 
 }
+
 void specifier_o(char *buff, int n){
 	unsigned num = (unsigned int)n;
-	UDecInStr(buff, num);
+	UDecInNumSys(buff, num, 10);
+}
+
+void specifier_u(char *buff, int n){
+	unsigned num = (unsigned int)n;
+	UDecInNumSys(buff, num, 8);
+}
+
+void specifier_x(char *buff, int n){
+	unsigned num = (unsigned int)n;
+	UDecInNumSys(buff, num, 16);
+}
+
+void specifier_X(char *buff, int n){
+	unsigned num = (unsigned int)n;
+	UDecInNumSys(buff, num, 16);
 }
--- src/test.c
+++ src/test.c
@@ -6,21 +6,30 @@ void shift_str(char *str, int size){
 		str[i] = str[i - 1];
 	}
 }
-void UDecInStr(char *buff, unsigned int n){
+void UDecInNumSys(char *buff, unsigned int n, int mes){
 	int size_ans = 0;
 	while(n != 0){
 		size_ans += 1;
 		shift_str(buff, size_ans);
-		buff[0] = n % 10 + '0';
-		n /= 10;
+		if(n % mes > 9){
+			buff[0] = n % mes - 10 + 'A';
+		}else{
+			buff[0] = n % mes + '0';
+		}
+		n /= mes;
 	}
 	buff[size_ans + 1] = '\0';
 
 }
 
+void specifier_o(char *buff, int n){
+	unsigned num = (unsigned int)n;
+	UDecInNumSys(buff, num, 10);
+}
+
 int main(){
     unsigned a = 1956;
     char buff[512];
-    UDecInStr(buff, a);
-    printf("%s", buff);
+    UDecInNumSys(buff, a, 2);
+    printf("%g", 456.5600001);
 }
