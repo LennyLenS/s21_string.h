3167d06ee0e4b97608f1bf74df20d39084c0bddb
Julia <yulya4ist@gmail.com>
2023-02-14 23:25:26 +0300

    сделала краисвее

--- src/flag_func.c
+++ src/flag_func.c
@@ -4,24 +4,31 @@
 
 char *add_sign(char *buf_str);
 
+char *main_func(char *str,  Prototype pr);
+
+char *add_char(char *str, int n, char *c);
+
+char *add_sign(char *buf_str);
+
 // %[флаги][ширина][.точность][длина]спецификатор
 
 int main() {
     char *a = "1234";
 
     char str[256];
-    sprintf(str, "%d", 1234);
+    sprintf(str, "%1.10d", 1234);
     printf("%s.\n", str);
     Prototype pr = {0};
 
     pr.spec = 'd';
-    pr.minus_flag = 1; // выпавниваем по левому краю
-    pr.plus_flag = 1; // выводит знак 
-    pr.width_number = 10; // ширина
-    pr.prec_number = 1; // точность 
-    int n = strlen(a); // длина
+    pr.minus_flag = 0; // выпавниваем по левому краю
+    pr.plus_flag = 0; // выводит знак 
+    pr.width_number = 1; // ширина
+    pr.prec_number = 10; // точность 
+    // int n = strlen(a); // длина
 
-
+    printf("%s", main_func(a, pr));
+    /*
     if (n>=pr.width_number){
         if (n >=pr.prec_number) {
             printf("%s\n", a);
@@ -127,12 +134,156 @@ int main() {
             }
         }
     }
+    */
     return 0;
 }
 
-char *func(char *buf_str) {
+char *main_func(char *str,  Prototype pr) {
+    char *res;
+     int n = strlen(str);
+
+     if (n>=pr.width_number){
+        if (n >=pr.prec_number) {
+            // printf("%s\n", a);
+            res = str;
+        }
+        else if (n < pr.prec_number) {
+            // printf("xui");
+            int buf = pr.prec_number - n;
+            
+            char *buf_str;
+            buf_str = (char*)malloc(buf*sizeof(char));
+
+            buf_str = add_char(buf_str, buf, "0");
+
+            // for (int i=0; i<buf;i++) {
+            //     buf_str = strcat(buf_str, "0");
+            // }
+
+            // printf("buf_str =%s\n", buf_str);
+            if (pr.spec == 'd') {
+                if (pr.plus_flag == 1) {
+                    buf_str = add_sign(buf_str);
+                    //printf("%s\n", strcat(buf_str, a));
+                    return strcat(buf_str, str);
+                }
+                else {
+                     // printf("%s\n", strcat(buf_str, a));
+                     return  strcat(buf_str, str);
+                }
+            }
+               
+        }
+    }
+    else if (n < pr.width_number) {
+        if (n >= pr.prec_number) { // 2 case with minus flag
+            int buf = pr.plus_flag == 1 ? pr.width_number - n -1: pr.width_number - n;
+            char *buf_str;
+            buf_str = (char*)malloc(buf*sizeof(char));
+
+            // for (int i=0; i<buf;i++) {
+            //     buf_str = strcat(buf_str, " ");
+            // }
+             buf_str = add_char(buf_str, buf, " ");
+
+            // printf("buf_str =%s.\n", buf_str);
+            if (pr.spec == 'd') {
+               
+                if (pr.minus_flag == 1) {
+                     // printf("xui\n");
+                     char *res_buf;
+                     res_buf = (char*)malloc((n+buf-1)*sizeof(char));
+                     // res_buf = strcat(res_buf, res_buf);
+                     res_buf = strcat(res_buf, buf_str);
+                     if (pr.plus_flag == 1) {
+                         res_buf = add_sign(res_buf);
+                     }
+                     // printf("%s.\n", res);
+                     res = res_buf;
+                }
+                else {
+                     // printf("%s\n", strcat(buf_str, a));
+                     res = strcat(buf_str, str);
+                }
+                    
+            }
+               
+        }
+        else if (n < pr.prec_number) {
+            // точность и ширина больше длины - херачим пробелы и нули перед числом
+            if (pr.prec_number <= pr.width_number) { // 1 case with minus_flag
+                // printf("хуй\n");
+                //int buf = pr.width_number - n;
+                int buf = pr.plus_flag == 1 ? pr.width_number - n - 1: pr.width_number - n;
+
+                char *buf_str;
+                buf_str = (char*)malloc(buf*sizeof(char));
+                
+                // for (int i=0; i< pr.prec_number - n;i++) {
+                //     buf_str = strcat(buf_str, "0");
+                // }
+                buf_str = add_char(buf_str, buf, "0");
+
+                if (pr.minus_flag == 1) {
+                    strcat(buf_str, str);
+                }
+                
+                    //  for (int i=0; i< buf -(pr.prec_number - n);i++) {
+                    //      buf_str = strcat(buf_str," ");
+                    // }
+                     buf_str = add_char(buf_str, buf, " ");
+                
+                if (pr.spec == 'd' && pr.minus_flag != 1) {
+                    // printf("%s\n", strcat(buf_str, str));
+                    res = strcat(buf_str, str);
+                }
+                else {
+                    if (pr.plus_flag == 1) {
+                         buf_str = add_sign(buf_str);
+                     }
+                    // printf("%s.\n", buf_str);
+                    res = buf_str;
+                }
+            }
+            else if (pr.prec_number > pr.width_number) {
+                // printf("хуй\n");
+                int buf = pr.prec_number - n;
+                char *buf_str;
+                buf_str = (char*)malloc(buf*sizeof(char));
+               
+                // for (int i=0; i< buf;i++) {
+                //     buf_str = strcat(buf_str,"0");
+                // } 
+                buf_str = add_char(buf_str, buf, "0");
+
+                // printf("buf_str =%s\n", buf_str);
+                if (pr.spec == 'd') {
+                    if (pr.plus_flag == 1) {
+                         buf_str = add_sign(buf_str);
+                     }
+                     // printf("%s\n", strcat(buf_str, a));
+                     res = strcat(buf_str, str);
+                }
+                   
+            }
+        }
+    }
+
+
+
+    return res;
+}
+
+char *add_sign(char *buf_str) {
     // char *res;
     memmove( buf_str + 1, buf_str, strlen( buf_str ) + 1 );
     memset( buf_str, '+', 1 );
     return buf_str;
+}
+
+char *add_char(char *str, int n, char *c) {
+     for (int i = 0; i < n;i++) {
+                 str = strcat(str, c);
+             }
+    return str;
 }
