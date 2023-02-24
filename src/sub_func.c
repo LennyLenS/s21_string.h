804177e252a0b18c9553beed779eabbb0c17c85f
Urtots Mauro <urtotsma@mi-a4.msk.21-school.ru>
2023-02-24 14:49:58 +0300

    1

--- src/sub_func.c
+++ src/sub_func.c
@@ -1,7 +1,7 @@
 #include "sub_func.h"
 
 void shift_str(char *str, int size){
-	for(int i = size_ans - 1; i > 0; -i){
+	for(int i = ans - 1; i > 0; -i){
 		str[i] = str[i - 1];
 	}
 }
@@ -24,29 +24,100 @@ void UDecInNumSys(char *buff, unsigned long long int n, int mes, int flag){     
 	buff[size_ans] = '\0';
 }
 
-void specifier_o(char *buff, int n){
-	unsigned num = (unsigned int)n;
-	UDecInNumSys(buff, num, 10);
+void specifier_o(char *buff, va_list args, Prototype prot){
+	if(prot.length == 'L'){
+		unsigned long int num = va_arg(args, unsigned long int);
+		UDecInNumSys(buff, num, 10, 0);
+	}
+	else{
+		unsigned int num = va_arg(args, unsigned int);
+		UDecInNumSys(buff, num, 10, 0);
+	}
 }
 
-void specifier_u(char *buff, int n){
-	unsigned num = (unsigned int)n;
-	UDecInNumSys(buff, num, 8);
+void specifier_u(char *buff, va_list args, Prototype prot){
+	if(prot.length == 'L'){
+		unsigned long int num = va_arg(args, unsigned long int);
+		UDecInNumSys(buff, num, 8, 0);
+	}
+	else{
+		unsigned int num = va_arg(args, unsigned int);
+		UDecInNumSys(buff, num, 8, 0);
+	}
 }
 
-void specifier_x(char *buff, int n){
-	unsigned num = (unsigned int)n;
-	UDecInNumSys(buff, num, 16);
+void specifier_x(char *buff, va_list args, Prototype prot){
+	if(prot.length == 'L'){
+		unsigned long int num = va_arg(args, unsigned long int);
+		UDecInNumSys(buff, num, 16, 0);
+	}
+	else{
+		unsigned int num = va_arg(args, unsigned int);
+		UDecInNumSys(buff, num, 16, 0);
+	}
 }
 
-void specifier_X(char *buff, int n){
-	unsigned num = (unsigned int)n;
-	UDecInNumSys(buff, num, 16);
+void specifier_X(char *buff, va_list args, Prototype prot){
+	if(prot.length == 'L'){
+		unsigned long int num = va_arg(args, unsigned long int);
+		UDecInNumSys(buff, num, 16, 1);
+	}
+	else{
+		unsigned int num = va_arg(args, unsigned int);
+		UDecInNumSys(buff, num, 16, 1);
+	}
 }
 
-void DoubleInStr(buff, a, prot.prec_number){
-	int a_int = (int)a;
-	
+void s21_double_to_str(double num, char *str, int pres) {
+	int count_before_dot = 0, index = 0;
+	double num1 = 0, num2 = num;
+	while(num >= 1){
+		num /= 10;
+		count_before_dot++;
+	}
+	for(int i = 0; i < count_before_dot; ++i){
+		num *= 10;
+		int y = (int)num;
+		num1 = num1 * 10 + y;
+		num -= y;
+		str[index++] = y + '0';
+	}
+	if(count_before_dot == 0){
+		str[index++] = '0';
+	}
+	str[index++] = '.';
+	num2 -= num1;
+	for(int i = 0; i < pres; ++i){
+		num2 *= 10;
+		int y = (int)num2;
+		num2 -= y;
+		str[index++] = y + '0';
+	}
+
+
+	num2 *= 10;
+	int y = (int)num2;
+	if(y >= 5){
+		int trans = 1;
+		for(int i = count_before_dot + pres; i > 0; --i){
+			if(str[i] != '.'){
+				int a = str[i] - '0';
+				str[i] = (a + trans) % 10 + '0';
+				trans = (a + 1) / 10;
+			}
+		}
+
+		if(trans == 1 && str[0] == '9'){
+			shift_str(str, count_before_dot + pres + 1);
+			str[1] = '0';
+			str[0] = '1';
+		}
+		else if(trans == 1){
+			str[0] = str[0] + 1;
+		}
+
+	}
+	str[index] = '\0';
 }
 
 void specifier_f(char *buff, va_list args, Prototype prot){
@@ -60,9 +131,3 @@ void specifier_f(char *buff, va_list args, Prototype prot){
 	}
 }
 
-
-fgfg{
-	if(p){
-
-	}
-}
