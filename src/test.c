abded9d0c15a27c95443938552c54a8385dd2f1d
Karin Kennos <karinken@mi-h2.msk.21-school.ru>
2023-03-01 22:56:34 +0300

    продолжить делать тесты для n

--- src/test.c
+++ src/test.c
@@ -3,25 +3,23 @@
 #include "s21_sprintf.h"
 int main()
 {
-<<<<<<< HEAD
- char str1[400];
-  char str2[400];
-  char *str3 = "test: %-+.1Lf!\ntest: %- .2Lf!\ntest: %-0.3Lf!";
-  long double num = -0.999999;
+ char str1[100];
+  char str2[100];
+  char *str3 = "%o Test %o Te%-+#nst %o";
+  int val = 3015;
+  int val2 = 712;
+  int val3 = 99;
+  int valn1 = 0, valn2 = 0;
+
+// sprintf(str1, str3, val, val2, &valn1, val3);
+// s21_sprintf(str2, str3, val, val2, &valn2, val3);
+
 //   sprintf(str1, str3, num, num, num);
 //   s21_sprintf(str2, str3, num, num, num);
 
-   int a = sprintf(str1, str3, num, num, num);
-   int b = s21_sprintf(str2, str3, num, num, num);
-=======
-
-   char str1[400];
-  char str2[400];
-  char *str3 = "test: %- 23.14f!\ntest: %+ 25.15f!";
-  double num = 365289.34628654873789362746834;
-   int a = sprintf(str1, str3, num, num);
-   int b = s21_sprintf(str2, str3, num, num);
->>>>>>> 753c65899798ad048a723f3b61d3f695838be585
+   int a = sprintf(str1, str3, val, val2, &valn1, val3);
+   int b = s21_sprintf(str2, str3, val, val2, &valn2, val3);
+   
    printf("#%s#\n#%s#\n", str1, str2);
    printf("%d %d", (int)strlen(str1), (int)strlen(str2));
 }
