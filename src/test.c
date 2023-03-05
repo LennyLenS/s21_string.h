0a738be16013953d3474ecabaf15c04f34d64d2e
Urtots Mauro <urtotsma@mi-l6.msk.21-school.ru>
2023-03-05 17:28:43 +0300

    1

--- src/test.c
+++ src/test.c
@@ -6,13 +6,12 @@ int main()
 
  char str1[400];
   char str2[400];
-  char *str3 = "ж%20.10f\n%20.15f\n%-20.5fж";
-  double num = -76.756589;
+  char *str3 = "test: %.1Lf!\ntest: %.2Lf!\ntest: %.3Lf!";
+  long double num = 823631075973858585858447757573.6495633;
 
-   int a =  sprintf(str1, str3, num, num, num, num, num);
-   int b = s21_sprintf(str2, str3, num, num, num, num, num);
-   printf("#%s#\n#%s#\n", str1, str2);
-   printf("%d %d", (int)strlen(str1), (int)strlen(str2));
+  long int k = (long int)(num + 0.0000001f);
+  printf("%li\n", k);
+   
 }
 
 // 72363107597385858
---
+++ ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install gcc -y 
+sudo apt install make -y
+sudo apt install valgrind -y
+sudo apt install gcovr -y
+sudo apt install check -y
